a80a1ded4858db5c7de8f028acda2c5b101144f1
Rob Howard <rob@dailystory.com>
2024-06-14 15:02:09 -0500

    add support for redirect_url to web form shortcode

--- plugin/includes/class-dailystory-shortcodes.php
+++ plugin/includes/class-dailystory-shortcodes.php
@@ -18,9 +18,6 @@ class DailyStoryShortCodes {
     	    add_shortcode('ds-exitintent', array( 'DailyStoryShortCodes', 'dailystory_exit_intent_shortcode' ));
 	        // Add the [ds-popup] shortcode
     	    add_shortcode('ds-popup', array( 'DailyStoryShortCodes', 'dailystory_popup_shortcode' ));
-	        // Add the [ds-test] shortcode
-    	    add_shortcode('ds-test', array( 'DailyStoryShortCodes', 'dailystory_webform_test' ));
-    	    
         }
     }
 	// ──────────────────────────────────────────────
@@ -97,20 +94,31 @@ class DailyStoryShortCodes {
     	$atts = array_change_key_case((array)$atts, CASE_LOWER);
  
     	// override default attributes with user attributes
-    	$webform_id = shortcode_atts(['id' => '0',], $atts, $tag);
-		$webform_id = esc_html__($webform_id['id'], 'ds-webform') ;
+		$atts = shortcode_atts(['id' => '0', 'redirect_url' => ''], $atts);
+		$webform_id = esc_html__($atts['id'], 'ds-webform');
+		$redirect_url = esc_url_raw($atts['redirect_url']);
+
 		// Add the script reference, pulled from DailyStory, but eventually will be served from a CDN
 		wp_register_script('ds-landingpages', 'https://pages.dailystory.com/bundles/dailystory-landingpage', null,'2.0.6', true);
 		wp_enqueue_script('ds-landingpages');
+
 		// enqueue css
 		wp_enqueue_style('ds-webform','https://forms.dailystory.com/content/hosted-webform-min', null, '2.0.6', 'all');
-		// get the tenant uid
+
+			// get the tenant uid
     	$options = get_option('dailystory_settings');
 		$tenantuid = $options['dailystory_tenant_uid'];
+
+		// Build the URL with query string
+		$url = 'https://forms.dailystory.com/webform/' . $tenantuid . '/' . $webform_id;
+		if (!empty($redirect_url)) {
+			$url = add_query_arg('redirectUrl', $redirect_url, $url);
+		}
+
 		// get the contents
-		list($cc_result, $cc_error) = self::get_data('https://forms.dailystory.com/webform/' . $tenantuid . '/' . $webform_id);
+		list($cc_result, $cc_error) = self::get_data($url);
+
 		return $cc_result;
-	}    
-	
+	}
 }
 ?>
